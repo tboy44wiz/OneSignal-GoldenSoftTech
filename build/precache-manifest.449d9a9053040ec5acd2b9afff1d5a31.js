44e9daf799ca795b496606e477fef45e01f18921
tMan44wiz <tosolife@yahoo.com>
2020-09-12 18:07:30 +0100

    OneSignal GoldenSoftTech 3.

---
+++ build/precache-manifest.449d9a9053040ec5acd2b9afff1d5a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9acf477dcec94ff2236aa69bbb31d8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda09b46a4d8c81fe45a",
+    "url": "/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "fb5c84e036f587782151",
+    "url": "/static/js/2.37abc216.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37abc216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda09b46a4d8c81fe45a",
+    "url": "/static/js/main.c66de55c.chunk.js"
+  },
+  {
+    "revision": "0e633fd500323bd1eed9",
+    "url": "/static/js/runtime-main.11f0aa03.js"
+  }
+]);
